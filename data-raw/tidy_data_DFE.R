803bd0452716847e7436f399ddeebee3d3f9cc95
Yoann Anciaux <anciaux.yoann@gmail.com>
2021-04-02 17:33:11 +0200

    Update data
    Remove all the "low" expression level data and keep only the "normal" ones.

--- data-raw/tidy_data_DFE.R
+++ data-raw/tidy_data_DFE.R
@@ -19,11 +19,12 @@ common_Position_AA <- raw_data %>%
   filter(n == n_distinct(raw_data$Treatment))
 
 # Extracts only the rows with common Position and AA across all the Treatment
-data_filtered <- semi_join(raw_data, common_Position_AA, by = c("Position", "AA"))
+data_filtered <- semi_join(raw_data, common_Position_AA, by = c("Position", "AA")) %>%
+  filter(Expression_level == "normal")
 
 # Compute the mean of replicate and the variance of the error for each combination Position-AA in each Environment
 average_sd <- function(x) {sqrt(sum(x^2)) / length(x)} # function to compute the standard deviation of the mean.
-data_DFE <- data_filtered %>%
+tidy_data <- data_filtered %>%
   mutate(sd = abs(Upper_CI - Low_CI) / 4) %>% #approximation of the standard deviation by the standard error.
   filter(AA != "WILDTYPE") %>% # remove the wildtype from the data
   # group_by(Treatment) %>%
@@ -33,6 +34,12 @@ data_DFE <- data_filtered %>%
   group_by(Environment, Position, AA, Expression_level) %>%
   mutate(mean_s = mean(s), # mean of selection coefficients over the replicates per environmentXpositionXAA
          mean_sd = average_sd(sd)) # standard deviation of the measurement error over the replicates per environmentXpositionXAA
+
+# Reduce the data set to the means only
+data_DFE <- tidy_data %>%
+  ungroup() %>%
+  transmute(Position, AA, Environment, mean_s, mean_sd) %>%
+  distinct()
 
 write_tsv(data_DFE, here("data-raw", "tidy_data.tsv"))
 use_data(data_DFE, overwrite = TRUE)
